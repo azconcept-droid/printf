f210899c3a0865f125027fd3fe6fedc28af25e6b
Adesina Mubarak <adesinamubarak123@gmail.com>
2022-04-22 12:53:19 +0100

    printf cloning

--- main.h
+++ main.h
@@ -1,25 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+int _printf(const char *format, ...);
+int print_char(va_list ap, char *text, unsigned int ntext);
+int print_str(va_list ap, char *text, unsigned int ntext);
+int print_text(char *text, unsigned int ntext);
+
 
 /**
- * Struct display - Struct for display function
+ * struct display- struct for display function
  * @format_sp: format specifier
- * @f: pointer to display functions
+ * @f: pointer to display func
  */
 
-typedef Struct display
+typedef struct display
 {
 	char *format_sp;
 	int (*f)(va_list, char*, unsigned int);
 } display_s;
-
-int _printf(const char *format, ...);
-int print_c(va_list ap, char *text, unsigned int ntext);
-int print_s(va_list ap, char *text, unsigned int ntext);
-int (*get_function())(va_list ap, char *text, unsigned int ntext);
-
 #endif
