c781c60bdabdd4f140ef87021f30cd0043b2db0f
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-23 02:41:58 +0000

    main.h file

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 int _printf(const char *format, ...);
 int print_c(va_list ap, char *buf, unsigned int i_buf);
 int print_s(va_list ap, char *buf, unsigned int i_buf);
-int print_buf(char *buf, unsigned int i_buf);
+int print_buf(char *buf, unsigned int nbyte);
 int control_buf(va_list ap, char *buf, unsigned int i_buf);
 int (*get_sp_func(const char *s, int index))(va_list, char *, unsigned int);
 int count_sp(const char *arg_i, int index);
