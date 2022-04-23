5eacaa50a76a6a404b28b02a590cdad7aa7416d4
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-23 03:11:59 +0000

    main.h file

--- main.h
+++ main.h
@@ -12,6 +12,7 @@ int print_buf(char *buf, unsigned int nbyte);
 int control_buf(va_list ap, char *buf, unsigned int i_buf);
 int (*get_sp_func(const char *s, int index))(va_list, char *, unsigned int);
 int count_sp(const char *arg_i, int index);
+
 /**
  * struct specifier- struct for specifier function
  * @format_sp: format specifier
