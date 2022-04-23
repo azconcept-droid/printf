6151a8c3677b07d20885a8971e4d456615ab9be7
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-23 01:13:31 +0000

    add put_c.c

--- put_c.c
+++ put_c.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * print_chr - writes the character c to stdout
+ * @arguments: input char
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  *
+ * Return: 1.
  */
+int print_c(va_list ap, char *buf, unsigned int i_buf)
+{
+	char c;
 
+	c = va_arg(ap, int);
+	control_buf(buf, c, i_buf);
+
+	return (1);
+}
